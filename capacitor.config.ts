f01cc0ae0939c2c29de55958d3afece6a5c56642
Roark-Aerospace <info@roark-aerospace.com>
2025-08-15 15:55:19 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,5 +1,5 @@
 
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/core @capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'app.lovable.9bf6d4d481ef4a6b8c9260342f66d66d',
