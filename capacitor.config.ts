4e1725d407289754e587ba0b5e0e558bcde93776
Roark-Aerospace <info@roark-aerospace.com>
2025-08-15 17:14:42 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@
 import { CapacitorConfig } from '@capacitor/core @capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.ddaas-companion',
+  appId: 'app.lovable.9bf6d4d481ef4a6b8c9260342f66d66d',
   appName: 'Roark DDaaS Companion',
   webDir: 'dist',
   bundledWebRuntime: false,
